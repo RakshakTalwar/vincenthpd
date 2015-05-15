be69753c486c50a62ea682e821204b397509f26f
RakshakTalwar <rakshak@raptorbird.com>
2015-05-14 21:49:27 -0500

    databse complete

--- main.py
+++ main.py
@@ -5,41 +5,51 @@ Copyright (c) 2015 Rakshak Talwar
 import datetime, math, os, time
 import numpy as np
 import json, sqlite3
-from sklearn import cross_validation
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.neighbors import KNeighborsRegressor
 import vincent
 start_time = time.time()
 
 crime_database_file = 'crime_records.db'
 
-beat_mapper = vincent.Mapper()
 type_mapper = vincent.Mapper()
-
-cdb = vincent.Crime_db() #create the Crime_db instance
+beat_mapper = vincent.Mapper()
 
 #create and add crime instances to the database
 db_con = sqlite3.connect(crime_database_file)
 db_cur = db_con.cursor()
 db_cur.execute('SELECT cTime, OffenseType, Beat, NumOffenses FROM HPDCrimes ORDER BY cTime ASC')
 
-#convert the OffenseTypes and Beats to hashes
 all_data_from_sql = []
-first_crime = db_cur.fetchone() #grab the first crime
-base_time = #this is base line from where time will be referenced
 
-for crime in db_cur.fetchall():
+base_time = 0.0 #this is base line from where time will be referenced
+for ctr, crime in enumerate(db_cur.fetchall()):
     temp_list = []
-    temp_list.append()
+    #find the base time
+    if ctr == 0:
+        base_time = float(crime[0])
+    #add the time with respect to base_time
+    temp_list.append(crime[0] - base_time)
+    #convert the OffenseTypes and Beats to hashes
+    temp_list.append(type_mapper.get_hash(crime[1]))
+    temp_list.append(beat_mapper.get_hash(crime[2]))
+    #add the number of offenses
+    temp_list.append(crime[3])
+    #append this temporary list to the main list which stores all the data
+    all_data_from_sql.append(temp_list)
 
-major_array = np.vstack(db_cur.fetchall())
+major_array = np.vstack(all_data_from_sql)
 split_major_array = np.hsplit(major_array, 4)
 
 X_data = np.hstack((split_major_array[0], split_major_array[1], split_major_array[2]))
 y_data = np.ravel(split_major_array[3])
 
+print type(y_data)
+
+"""
 scores = []
-skf = cross_validation.StratifiedKFold(y_data, n_folds = 3) #create cross validation model
-neigh = KNeighborsClassifier(n_neighbors=5) #create KNN model
+skf = StratifiedKFold(y_data, n_folds = 3) #create cross validation model
+neigh = KNeighborsRegressor(n_neighbors=5) #create KNN model
 for train_index, test_index in skf:
     X_train, X_test = X_data[train_index], X_data[test_index]
     y_train, y_test = y_data[train_index], y_data[test_index]
@@ -65,13 +75,13 @@ for time_i in times:
     for beat_id in beatMapper.hash_to_key:
         temp_dict = {
             'date' : time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_i+base_time)),
-            'beat': beatMapper.get_key(beat_id),
-            'type': typeMapper.get_key(neigh.predict([time_i, beat_id])[0])
+            'beat': beat_mapper.get_key(beat_id),
+            'type': type_mapper.get_key(neigh.predict([time_i, beat_id])[0])
             }
 
         fut_week_crimes_list.append(temp_dict)
 fut_week_crimes["crimes"] = fut_week_crimes_list
 with open('future.json', 'w') as fl:
     json.dump(fut_week_crimes, fl)
-
+"""
 print 'time to complete: %ds' % (time.time() - start_time)
